f89b4a39a402038cde8dd6be1f43d53fa3017308
Your Name <dhatrakchhaya766@gmail.com>
2019-10-16 23:12:38 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+var $ = jQuery.noConflict();
+
+"use strict";
+
+
+/* for navbar menu*/
+jQuery(document).ready(function($) {
+    jQuery('.stellarnav').stellarNav({
+        theme: 'light'
+    });
+});
